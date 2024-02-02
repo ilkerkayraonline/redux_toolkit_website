82d83768df457e62feb1974d9c370f8c2c5c2c8c
ilkerkayraonline <ilkerkayra59@gmail.com>
2024-02-02 22:16:25 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+link : https://www.youtube.com/watch?v=h71OYALoFOI&t=172s   # redux_toolkit_website
---
+++ src/components/Navbar/Home/SliderComp.jsx
@@ -0,0 +1,39 @@
+import React from 'react'
+import Slider from 'react-slick';
+
+const SliderComp = () => {
+  const settings = {
+   // dots: true,
+    infinite: true,
+    speed: 300,
+    autoplay: true,
+    slidesToShow: 1,
+    slidesToScroll: 1
+  };
+  return (
+    <div>
+      <Slider {...settings}>
+          <div className='!flex items-center bg-gray-100 px-4'>
+            <div>
+              <div className='text-6xl font-bold'>En Kaliteli Ayakkabılar Burada</div>
+              <div className='text-lg my-4'>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Cupiditate at similique. Aliquid, ipsa voluptatum.</div>
+              <div className='border rounded-full cursor-pointer text-2xl w-[200px] h-16 flex items-center justify-center bg-gray-200'>İncele</div>
+            </div>
+            <img className='w-[450px] h-[450px]' src="./images/img1.png" alt="" />
+          </div>
+
+          <div className='!flex items-center bg-gray-100 px-4'>
+            <div>
+              <div className='text-6xl font-bold'>En Kaliteli Ayakkabılar Burada</div>
+              <div className='text-lg my-4'>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Cupiditate at similique. Aliquid, ipsa voluptatum.</div>
+              <div className='border rounded-full cursor-pointer text-2xl w-[200px] h-16 flex items-center justify-center bg-gray-200'>İncele</div>
+            </div>
+            <img className='w-[450px] h-[450px]'  src="./images/img2.jpg" alt="" />
+          </div>
+          
+        </Slider>
+    </div>
+  )
+}
+
+export default SliderComp
